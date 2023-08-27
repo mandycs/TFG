74dd4e093fc60dcd7086798ce13028d749853c22
mandycs <mandycortessimon@gmail.com>
2023-08-27 17:51:54 +0200

    Global variables and combat function

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import spacy
 import random
 import requests
+
 api_url = "https://api.openai.com/v1/engines/davinci-codex/completions"
+
 api_key = "YOUR_API_KEY"
 
 headers = {
@@ -9,12 +11,23 @@ headers = {
     "Authorization": f"Bearer {api_key}"
 }
 
-prompt_first_round = ""
+nlp = spacy.load("en_core_web_lg")
+
+verbal_forms = {"VBG":"Present continous","VBN":"Past participle","VBD":"Past simple","VB":"Base form","VBZ":"Present simple(3rd Person)","VBP":"Present simple","VH":"Future","VHD":"Past perfect","VHN":"Past Participle","VHP":"Present Perfect","VHZ":"Present Perfect(3rd person)","VVN":"Past Participle"}
 
 verbs_most_used = ["be","have","do","make","use","say","get","go","take","see","know","include","come","find","give","think","work","need","look","want","provide","help","become","start","follow","show","call","try","create","keep","leave","write","tell","play","add","feel","run","read","allow","put","mean","seem","lead","set","offer","ask","bring","hold","build","require","continue","learn","live","move","begin","like","receive","let","support","develop","consider","change","base","turn","pay","believe","meet","love","increase","happen","grow","serve","send","understand","remain","hear","lose","appear","accord","buy","win","expect","involve","produce","choose","speak","cause","improve","open","apply","talk","report","spend","join","sell","cover","enjoy","pass","reduce","stop","die"]
 
-verbal_forms = {"VBG":"Present continous","VBN":"Past participle","VBD":"Past simple","VB":"Base form","VBZ":"Present simple(3rd Person)","VBP":"Present simple","VH":"Future","VHD":"Past perfect","VHN":"Past Participle","VHP":"Present Perfect","VHZ":"Present Perfect(3rd person)","VVN":"Past Participle"}
-nlp = spacy.load("en_core_web_lg")
+prompt_first_round = ""
+
+global_protagonist = None
+
+global_enemy = None
+
+def game_instructions():
+    print("Welcome to the Legend of the Verbs!\nIn this game, you will control a protagonist and engage in battles with enemies")
+    print("You will learn how to use the modal verbs\nYou will have to attack the enemies using modal verbs")
+    print("You will have to use the correct modal verb to defeat the enemy\nIf you use the wrong modal verb, you will lose your turn")
+    print("You will have 100HP and the enemy will have HP according to the difficulty level")
 
 def choose_tense_and_verb():
     verb_chosen = random.choice(verbs_most_used)
@@ -42,7 +55,7 @@ class Character:
         self.hp = hp
         self.atk = atk
 
-    def take_damage(self, damage):  # damage is an integer      
+    def take_damage(self, damage): 
         self.hp -= damage
 
     def attack(self, enemy):
@@ -53,8 +66,18 @@ class Protagonist(Character):
         super().__init__(name, hp=100, atk=20)
 
 class Enemy(Character):
-    def __init__(self, name, difficulty):
-        super().__init__(name, hp = 50 * difficulty, atk = 10 * difficulty)
+    def __init__(self, difficulty):
+        super().__init__(hp = 50 * difficulty, atk = 10 * difficulty)
+
+def combat(success):
+    global global_protagonist
+    global global_enemy
+    if success == True:
+        global_protagonist.attack(global_enemy)
+        global_enemy.take_damage(global_protagonist.atk)
+    else:
+        global_enemy.attack(global_protagonist)
+        global_protagonist.take_damage(global_enemy.atk)
         
 def check_tense(input_text,form_chosen_nlp,verb_chosen):
     doc = nlp(input_text)
@@ -69,30 +92,18 @@ def check_tense(input_text,form_chosen_nlp,verb_chosen):
             print("You used the wrong verb")
             return False
 
-def level_1(protagonist, enemy):
+def level_1():
+    global_enemy = Enemy(1)
     verb_result,form_result,form_nlp_result = choose_tense_and_verb()
     print(api_gpt_call(prompt_first_round,verb_result,form_result))
     input_text = input("Enter your phrase: ")
     success = check_tense(input_text,form_nlp_result,verb_result)
-    if success == True:
-        protagonist.attack(enemy)
-    else:
-        protagonist.take_damage(enemy.atk)
-    """print("You have completed Level 1!")"""
-
-# Define more levels and enemies for each level
-def game_instructions():
-    print("Welcome to the Legend of the Verbs!\nIn this game, you will control a protagonist and engage in battles with enemies")
-    print("You will learn how to use the modal verbs\nYou will have to attack the enemies using modal verbs")
-    print("You will have to use the correct modal verb to defeat the enemy\nIf you use the wrong modal verb, you will lose your turn")
-    print("You will have 100HP and the enemy will have HP according to the difficulty level")
+    combat(success)
 
 def main():
     protagonist_name = input("Enter the name of your protagonist: ")
-    protagonist = Protagonist(protagonist_name)
-
-    level_1(protagonist)
-    # Call more level functions here
+    global_protagonist = Protagonist(protagonist_name)
+    level_1()
 
 if __name__ == "__main__":
     main()
